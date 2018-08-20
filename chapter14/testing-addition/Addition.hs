3f978fdea22e1383a212dc00f018b1889af8637c
brendon <brendon.soto@gmail.com>
2018-08-19 20:52:15 -0400

    Work in progress -- Chapter 14

--- chapter14/testing-addition/Addition.hs
+++ chapter14/testing-addition/Addition.hs
@@ -1,6 +1,7 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
 
 dividedBy :: Integral a => a -> a -> (a, a)
 dividedBy num denom = go num denom 0
@@ -8,6 +9,59 @@ dividedBy num denom = go num denom 0
           | n < d = (count ,n)
           | otherwise =
               go (n - d) d (count + 1)
+
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b)
+         => Gen (a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c)
+            => Gen (a, b, c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b)
+          => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return (Just a))]
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
+
 
 main :: IO ()
 main = hspec $ do
@@ -21,3 +75,6 @@ main = hspec $ do
     it "22 divided by 5 is\
        \ 4 remainder 2" $ do
        dividedBy 22 5 `shouldBe` (4, 2)
+    it "x + 1 is always\
+       \ greater than x" $ do
+       property $ \x -> x + 1 > (x :: Int)
