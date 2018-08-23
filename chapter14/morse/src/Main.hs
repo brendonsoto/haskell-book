257f0ae80d18bf01d472348aac5fdb3c1f84cf07
brendon <brendon.soto@gmail.com>
2018-08-23 10:18:48 -0400

    Just reached the exercises of chapter 14

--- chapter14/morse/src/Main.hs
+++ chapter14/morse/src/Main.hs
@@ -7,8 +7,101 @@ import Morse (stringToMorse, morseToChar)
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
 import System.IO (hGetLine, hIsEOF, stdin)
+import Test.QuickCheck
+import Test.QuickCheck (oneof)
 
 
+-- QuickCheck stuff
+data Trivial =
+  Trivial
+  deriving (Eq, Show)
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = trivialGen
+
+data Identity a =
+  Identity a
+  deriving (Eq, Show)
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do
+  a <- arbitrary
+  return (Identity a)
+
+instance Arbitrary a =>
+         Arbitrary (Identity a) where
+  arbitrary  = identityGen
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+-- Arbitrary product types
+data Pair a b =
+  Pair a b
+  deriving (Eq, Show)
+
+pairGen :: (Arbitrary a,
+            Arbitrary b) =>
+            Gen (Pair a b)
+pairGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Pair a b)
+
+instance (Arbitrary a,
+          Arbitrary b) =>
+         Arbitrary (Pair a b) where
+  arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
+-- Sum Types
+data Sum a b =
+    First a
+  | Second b
+  deriving (Eq, Show)
+
+-- equal odds for each
+sumGenEqual :: (Arbitrary a,
+                Arbitrary b) =>
+               Gen (Sum a b)
+sumGenEqual = do
+  a <- arbitrary
+  b <- arbitrary
+  oneof [return $ First a,
+         return $ Second b]
+
+sumGenCharInt :: Gen (Sum Char Int)
+sumGenCharInt = sumGenEqual
+
+-- assigining different weights to frequency
+sumGenFirstPls :: (Arbitrary a,
+                   Arbitrary b) =>
+                  Gen (Sum a b)
+sumGenFirstPls = do
+  a <- arbitrary
+  b <- arbitrary
+  frequency [(10, return $ First a),
+             (1, return $ Second b)]
+
+sumGenCharIntFirst :: Gen (Sum Char Int)
+sumGenCharIntFirst = sumGenFirstPls
+
+
+-- -- CoArbitrary
+-- arbitrary :: Arbitrary a =>
+--              Gen a
+-- coarbitrary :: CoArbitrary a =>
+--                a -> Gen b -> Gen b
+--
+
+
+
+-- Main code
 convertToMorse :: IO ()
 convertToMorse = forever $ do
   weAreDone <- hIsEOF stdin
@@ -51,21 +144,27 @@ convertFromMorse = forever $ do
           exitFailure
 
 
+-- main :: IO ()
+-- main = do
+--   mode <- getArgs
+--   case mode of
+--     [arg] ->
+--       case arg of
+--         "from" -> convertFromMorse
+--         "to"   -> convertToMorse
+--         _      -> argError
+--     _ -> argError
+--
+--   where argError = do
+--           putStrLn "Please specify the\
+--                    \ first argument\
+--                    \ as being 'from' or\
+--                    \ 'to' morse,\
+--                    \ such as: morse to"
+--           exitFailure
+
 main :: IO ()
 main = do
-  mode <- getArgs
-  case mode of
-    [arg] ->
-      case arg of
-        "from" -> convertFromMorse
-        "to"   -> convertToMorse
-        _      -> argError
-    _ -> argError
+  sample trivialGen
 
-  where argError = do
-          putStrLn "Please specify the\
-                   \ first argument\
-                   \ as being 'from' or\
-                   \ 'to' morse,\
-                   \ such as: morse to"
-          exitFailure
+
