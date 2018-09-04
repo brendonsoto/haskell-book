5529d04969a491ba5aee6bb8ca44530e0efa0874
brendon <brendon.soto@gmail.com>
2018-09-04 08:34:07 -0400

    Ch14 Quickcheck exercises 3,4,5

--- chapter14/exercises/src/ArithmeticTesting.hs
+++ chapter14/exercises/src/ArithmeticTesting.hs
@@ -26,5 +26,54 @@ prop_listOrdered xs = listOrdered . sort $ xs
 -- prop_listOrdered = listOrdered -- NOTE This will produce failing results!
 
 
-runQc :: IO ()
-runQc = quickCheck prop_listOrdered
+-- runQc :: IO ()
+-- runQc = quickCheck prop_listOrdered
+
+
+-- 3
+plusAssociative :: Int -> Int -> Int -> Bool
+plusAssociative x y z =
+    x + (y + z) == (x + y) + z
+
+plusCommutative :: Int -> Int -> Bool
+plusCommutative x y =
+    x + y == y + x
+
+
+runQc3 :: IO ()
+runQc3 = do
+    quickCheck plusAssociative
+    quickCheck plusCommutative
+
+
+-- 4
+multAssociative :: Int -> Int -> Int -> Bool
+multAssociative x y z =
+    x * (y * z) == (x * y) * z
+
+multCommutative :: Int -> Int -> Bool
+multCommutative x y =
+    x * y == y * x
+
+
+runQc4 :: IO ()
+runQc4 = do
+    quickCheck multAssociative
+    quickCheck multCommutative
+
+
+-- 5
+-- TODO Check to see if there's a way to test numbers greater than 0
+prop_quotRem :: Int -> Int -> Bool
+prop_quotRem x y =
+    (quot x y) * y + (rem x y) == x
+
+prop_divMod :: Int -> Int -> Bool
+prop_divMod x y =
+    (div x y) * y + (mod x y) == x
+
+
+runQc5 :: IO ()
+runQc5 = do
+    quickCheck prop_quotRem
+    quickCheck prop_divMod
