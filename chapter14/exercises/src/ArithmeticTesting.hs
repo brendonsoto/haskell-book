86dec28bc5728b342c8d416be9d00fe999216ac7
brendon <brendon.soto@gmail.com>
2018-09-03 20:17:04 -0400

    Got through QuickCheck ex #2

--- chapter14/exercises/src/ArithmeticTesting.hs
+++ chapter14/exercises/src/ArithmeticTesting.hs
@@ -1,5 +1,6 @@
 module ArithmeticTesting where
 
+import Data.List (sort)
 import Test.QuickCheck
 
 half :: Fractional a => a -> a
@@ -11,6 +12,19 @@ prop_half :: Float -> Bool
 prop_half x = halfIdentity x == x
 
 
+-- 2
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered xs =
+  snd $ foldr go (Nothing, True) xs
+  where go _ status@(_, False) = status
+        go y (Nothing, t) = (Just y, t)
+        go y (Just x, t) = (Just y, x >= y)
+
+
+prop_listOrdered :: [Char] -> Bool
+prop_listOrdered xs = listOrdered . sort $ xs 
+-- prop_listOrdered = listOrdered -- NOTE This will produce failing results!
+
 
 runQc :: IO ()
-runQc = quickCheck prop_half
+runQc = quickCheck prop_listOrdered
