4073124686f513f1219aa1017aefda800b40f626
brendon <brendon.soto@gmail.com>
2018-09-04 20:39:27 -0400

    Completed ch14 quickcheck exercises 6, 7, and 8.

--- chapter14/exercises/src/ArithmeticTesting.hs
+++ chapter14/exercises/src/ArithmeticTesting.hs
@@ -63,13 +63,14 @@ runQc4 = do
 
 
 -- 5
--- TODO Check to see if there's a way to test numbers greater than 0
-prop_quotRem :: Int -> Int -> Bool
-prop_quotRem x y =
+-- NOTE: NonZero I found on github here: https://github.com/CarlosMChica/HaskellBook/blob/a0cd94b94e5065c6cb36c54b1c9bb6a73f551ec1/chapter14/arithmetic/test/Spec.hs
+-- QuickCheck doc: https://hackage.haskell.org/package/QuickCheck-2.8.2/docs/Test-QuickCheck.html#t:NonZero
+prop_quotRem :: NonZero Int -> NonZero Int -> Bool
+prop_quotRem (NonZero x) (NonZero y) =
     (quot x y) * y + (rem x y) == x
 
-prop_divMod :: Int -> Int -> Bool
-prop_divMod x y =
+prop_divMod :: NonZero Int -> NonZero Int -> Bool
+prop_divMod (NonZero x) (NonZero y) =
     (div x y) * y + (mod x y) == x
 
 
@@ -77,3 +78,51 @@ runQc5 :: IO ()
 runQc5 = do
     quickCheck prop_quotRem
     quickCheck prop_divMod
+
+
+-- 6
+prop_expAssoc :: Int -> Int -> Int -> Bool
+prop_expAssoc x y z = x ^ (y ^ z) == (x ^ y) ^ z
+
+prop_expCommutative :: Int -> Int -> Bool
+prop_expCommutative x y = x ^ y == y ^ x
+
+runQc6 :: IO ()
+runQc6 = do
+    quickCheck prop_expAssoc
+    quickCheck prop_expCommutative
+
+
+-- 7
+prop_doubleReversed :: Eq a => [a] -> Bool
+prop_doubleReversed xs = (reverse . reverse $ xs) == id xs
+
+prop_doubleReversed_int :: [Int] -> Bool
+prop_doubleReversed_int = prop_doubleReversed
+
+prop_doubleReversed_char :: [Char] -> Bool
+prop_doubleReversed_char = prop_doubleReversed
+
+runQc7 :: IO ()
+runQc7 = do
+    quickCheck prop_doubleReversed_int
+    quickCheck prop_doubleReversed_char
+
+
+-- 8
+prop_application :: Eq a => (a -> a) -> a -> Bool
+prop_application f a = (f $ a) == f a
+
+prop_composition :: Eq a => (a -> a) -> (a -> a) -> a -> Bool
+prop_composition f g x = (f . g $ x) == f (g x)
+
+prop_app_int :: Int -> Bool
+prop_app_int = prop_application (+1)
+
+prop_comp_int :: Int -> Bool
+prop_comp_int = prop_composition (*2) (+3)
+
+runQc8 :: IO ()
+runQc8 = do
+    quickCheck prop_app_int
+    quickCheck prop_comp_int
